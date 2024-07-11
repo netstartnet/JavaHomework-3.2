df7bdd1df8a77bc4ca839c51c9c249d6cc3d1a97
netstartnet <gmodkru@gmail.com>
2024-07-11 18:01:28 +0300

    FixedTask

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int weight = 95;
         double height = 1.85;
         double bmi = service.calculate(weight, height);
-        String bmiResult = String.format("%.2f",bmi);
+        String bmiResult = String.format("%.0f",bmi);
         System.out.println("Ваш вес: " + weight + " кг." + " Ваш рост: " + height*100 + " см." + " Ваш ИМТ составляет: " + bmiResult);
     }
 }
