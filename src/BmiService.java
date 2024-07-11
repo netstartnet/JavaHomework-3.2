b2e9124c2cc7334df7393058fb0513f7272076db
netstartnet <gmodkru@gmail.com>
2024-07-11 18:50:38 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
-    public double calculate (int weight, double height){
-        double bmi = weight / (height*height);
+   public double calculate (int weight, double height){
+        double wth = weight / (height*height);
+        int bmi = (int)wth;
         return bmi;
     }
 }
